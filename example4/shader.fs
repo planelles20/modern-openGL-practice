de253e6fdd6be98986f44098ffca0ce805ab822f
Carlos Planelles <planelles20@gmail.com>
2017-05-12 12:12:24 +0200

    erase x and y variables (useless)

--- example4/shader.fs
+++ example4/shader.fs
@@ -8,10 +8,6 @@ out vec4 color;
 
 void main()
 {
-    float x = (gl_FragCoord.x - 1200.0f / 2.0f) / (1200.0f / 2.0f);
-    float y = (gl_FragCoord.y - 800.0f / 2.0f) / (800.0f / 2.0f);
-
-
     if(dot(gl_PointCoord-0.5,gl_PointCoord-0.5)>0.25)
         discard;
     else
