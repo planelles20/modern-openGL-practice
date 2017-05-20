bb3bd14c3661aec9b92c1e1e506ddcb3de10d8f5
Carlos Planelles <planelles20@gmail.com>
2017-05-20 17:03:26 +0200

    add example 12, move using mouse

---
+++ example12/README.md
@@ -0,0 +1,5 @@
+# Move using w, s, a, d, keys and mouse.
+
+<p align="center">
+  <img src="https://github.com/planelles20/modern-openGL-practice/blob/master/example12/result/result12.gif?raw=true" alt="grid screenshot figure"/>
+</p>
---
+++ example12/main.cpp
@@ -0,0 +1,199 @@
+#include <iostream>
+#include <math.h>
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW (include in display class)
+//#include <GLFW/glfw3.h>
+
+// GLM Mathematics
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// our class includes
+#include "../common/display.h"
+#include "../common/shader.h"
+#include "mesh.h"
+
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
+//mesh dimensions
+const unsigned int Xmesh = 10, Ymesh = 20;
+
+// Camera
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+GLfloat yaw    = -90.0f;	// Yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right (due to how Eular angles work) so we initially rotate a bit to the left.
+GLfloat pitch  =  0.0f;
+GLfloat lastX  =  WIDTH  / 2.0;
+GLfloat lastY  =  HEIGHT / 2.0;
+GLfloat fov =  45.0f;
+bool keys[1024];
+
+// Deltatime
+GLfloat deltaTime = 0.0f;	// Time between current frame and last frame
+GLfloat lastFrame = 0.0f;  	// Time of last frame
+
+void printInstructions(void);
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void do_movement();
+
+// Our main program
+int main() {
+    // create our window
+    Display display = Display(WIDTH, HEIGHT, "LearnOpenGL Carlos Planelles", key_callback, mouse_callback, scroll_callback);
+
+    // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+    glewExperimental = GL_TRUE;
+
+    // Initialize GLEW to setup the OpenGL Function pointers
+    glewInit();
+
+    // Define the viewport dimensions
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+
+    // Build and compile our shader program
+    Shader ourShader = Shader("shader.vs", "shader.fs");
+
+    // create our mesh grid class
+    Mesh mesh = Mesh(Xmesh, Ymesh);
+
+    // display keyboard instructions (like how to exit ...)
+    display.Instructions();
+    printInstructions();
+
+    //this call will result in wireframe polygons.
+    mesh.PolygonMode();
+
+    // game loop
+    while (display.State()) {
+        // Calculate deltatime of current frame
+        GLfloat currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        display.PollEvents();
+        do_movement();
+
+        mesh.BackGround(0.0, 0.0, 0.0, 1.0);
+
+        // use our shader
+        ourShader.Use();
+
+        // Camera/View transformation
+        glm::mat4 view;
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+        glm::mat4 model;
+        glm::mat4 projection;
+        model = glm::rotate(model, -45.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        projection = glm::perspective(45.0f, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+        // Get their uniform location
+        GLint modelLoc = glGetUniformLocation(ourShader.ThisProgram(), "model");
+        GLint viewLoc = glGetUniformLocation(ourShader.ThisProgram(), "view");
+        GLint projLoc = glGetUniformLocation(ourShader.ThisProgram(), "projection");
+        // Pass them to the shaders
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        // Note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        //draw mesh
+        mesh.Draw();
+
+        //Swap buffers
+        display.SwapWindows();
+
+    }
+
+    //NOTE: glfwTerminate(); is call in display destructor
+    return 0;
+}
+
+void printInstructions(void){
+    std::cout << "w: move forward" << '\n';
+    std::cout << "s: move backwards" << '\n';
+    std::cout << "d: move right" << '\n';
+    std::cout << "a: move left" << '\n';
+    std::cout << "use mouse too" << '\n';
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    if (key >= 0 && key < 1024)
+    {
+        if (action == GLFW_PRESS)
+            keys[key] = true;
+        else if (action == GLFW_RELEASE)
+            keys[key] = false;
+    }
+}
+
+void do_movement() {
+    // Camera controls
+    GLfloat cameraSpeed = 5.0f * deltaTime;
+    if (keys[GLFW_KEY_W])
+        cameraPos += cameraSpeed * cameraFront;
+    if (keys[GLFW_KEY_S])
+        cameraPos -= cameraSpeed * cameraFront;
+    if (keys[GLFW_KEY_A])
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (keys[GLFW_KEY_D])
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+bool firstMouse = true;
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    GLfloat xoffset = xpos - lastX;
+    GLfloat yoffset = lastY - ypos; // Reversed since y-coordinates go from bottom to left
+    lastX = xpos;
+    lastY = ypos;
+
+    GLfloat sensitivity = 0.05;	// Change this value to your liking
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw   += xoffset;
+    pitch += yoffset;
+
+    // Make sure that when pitch is out of bounds, screen doesn't get flipped
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+    if (fov >= 1.0f && fov <= 45.0f)
+        fov -= yoffset;
+    if (fov <= 1.0f)
+        fov = 1.0f;
+    if (fov >= 45.0f)
+        fov = 45.0f;
+}
